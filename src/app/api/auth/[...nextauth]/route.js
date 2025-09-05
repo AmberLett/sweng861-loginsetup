037fe02ee7294d50febb2d2cd9a5366fd90978a5
AmberLett <arl6269@psu.edu>
2025-09-04 23:55:38 -0400

    Adding all project files and update .gitignore

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import LinkedInProvider from "next-auth/providers/linkedin";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
